a206d81173ccce07fcc212de1456f27af7930eca
Therm Scissorpunch <61228862+ThermScissorpunch@users.noreply.github.com>
2024-12-20 12:19:39 +0100

    Remove unused var

--- components/nav/header.tsx
+++ components/nav/header.tsx
@@ -9,21 +9,6 @@ import { Icon } from "../icon";
 import NavItems from "./nav-items";
 import { useLayout } from "../layout/layout-context";
 
-const headerColor = {
-  default:
-    "text-black dark:text-white from-gray-50 to-white dark:from-gray-800 dark:to-gray-900",
-  primary: {
-    blue: "text-white from-blue-300 to-blue-500",
-    teal: "text-white from-teal-400 to-teal-500",
-    green: "text-white from-green-400 to-green-500",
-    red: "text-white from-red-400 to-red-500",
-    pink: "text-white from-pink-400 to-pink-500",
-    purple: "text-white from-purple-400 to-purple-500",
-    orange: "text-white from-orange-400 to-orange-500",
-    yellow: "text-white from-yellow-400 to-yellow-500",
-  },
-};
-
 export default function Header() {
   const { globalSettings, theme } = useLayout();
   const header = globalSettings.header;
